1ce6d76d21669564965233a2f1393940d957c2cc
seaze <seaze@se.cn>
2019-09-07 14:46:59 +0800

    fix 7.3

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-docker build -f Dockerfile.5.6.fpm.alpine -t php5.6-fpm-alpine .
-# docker build -f Dockerfile.7.3.fpm.alpine -t php7.3-fpm-alpine .
+# docker build -f Dockerfile.5.6.fpm.alpine -t php5.6-fpm-alpine .
+docker build -f Dockerfile.7.3.fpm.alpine -t php7.3-fpm-alpine .
