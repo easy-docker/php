cdd83eee9a9a58ace2c591d7b1762a0446a2856f
ghostry <y@1ge.fun>
2025-04-10 11:15:05 +0800

    Update start.sh

--- 8.1.nginx/start.sh
+++ 8.1.nginx/start.sh
@@ -4,7 +4,7 @@
 sed -i "s/\${PORT}/${PORT}/g" /etc/nginx/sites-available/default
 sed -i "s/\${PHPPORT}/${PHPPORT}/g" /etc/nginx/sites-available/default
 sed -i -E 's/(listen = 127.0.0.1:)[0-9]+/\1'"$PHPPORT"'/' /usr/local/etc/php-fpm.d/www.conf
-
+chown -R www-data:www-data /data
 # 启动 PHP-FPM 和 Nginx
 php-fpm &
 nginx -g "daemon off;"
