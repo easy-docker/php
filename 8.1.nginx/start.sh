199f461d58122bb1a1b75930b8f0f20dcc431d75
ghostry <y@1ge.fun>
2025-04-10 11:28:52 +0800

    Update start.sh

--- 8.1.nginx/start.sh
+++ 8.1.nginx/start.sh
@@ -3,7 +3,7 @@
 # 替换 nginx.conf 中的 ${PORT} 为实际的环境变量 PORT
 sed -i "s/\${PORT}/${PORT}/g" /etc/nginx/sites-available/default
 sed -i "s/\${PHPPORT}/${PHPPORT}/g" /etc/nginx/sites-available/default
-sed -i -E 's/(listen = )[0-9]+/\1'"${PHPPORT}"'/' /usr/local/etc/php-fpm.d/zz-docker.conf
+sed -i -E 's/(listen = 127.0.0.1:)[0-9]+/\1'"${PHPPORT}"'/' /usr/local/etc/php-fpm.d/www.conf
 chown -R www-data:www-data /data
 # 启动 PHP-FPM 和 Nginx
 php-fpm &
