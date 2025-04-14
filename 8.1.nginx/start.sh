8afcc53975e370802f53518261953ad024f2609a
ghostry <y@1ge.fun>
2025-04-14 16:53:00 +0800

    Update start.sh

--- 8.1.nginx/start.sh
+++ 8.1.nginx/start.sh
@@ -4,6 +4,8 @@
 sed -i "s/\${PORT}/${PORT}/g" /etc/nginx/sites-available/default
 sed -i "s/\${PHPPORT}/${PHPPORT}/g" /etc/nginx/sites-available/default
 sed -i -E 's/(listen = 127.0.0.1:)[0-9]+/\1'"${PHPPORT}"'/' /usr/local/etc/php-fpm.d/www.conf
+echo 'upload_max_filesize = 10M'>>/usr/local/etc/php/conf.d/docker-fpm.ini
+echo 'post_max_size = 10M'>>/usr/local/etc/php/conf.d/docker-fpm.ini
 chown -R www-data:www-data /data
 # 启动 PHP-FPM 和 Nginx
 php-fpm &
